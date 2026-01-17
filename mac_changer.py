f94f97e5703e80e32bafeebe3c04e7490ff69aa6
Elephant_On_Acid <162466929+shaiefhagelaar@users.noreply.github.com>
2026-01-17 17:41:34 +0100

    Update mac_changer.py
    Read comments in code

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,14 @@
 
 import subprocess
 
+#Turns off eth0
 subprocess.call(["ifconfig eth0 down"], shell=True)
-subprocess.call(["ifconfig eth0 down hw ether 00:11:22:33:44:55:66"], shell=True)
+
+#Writes new mac address
+subprocess.call(["ifconfig eth0 down hw ether 00:11:22:33:44:15"], shell=True)
+
+#Turns on eth0
 subprocess.call(["ifconfig eth0 up"], shell=True)
+
+#Displays new mac address
+subprocess.call(["ifconfig | grep eth0"], shell=True)
